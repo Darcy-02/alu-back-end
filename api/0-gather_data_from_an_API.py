4d66c521ab7250500513038d0cd5122537e0799d
Mbanza Teta Darcy <d.mbanza@alustudenty.com>
2025-07-21 08:10:01 -0700

    gathering data from an API

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# import libraries
+import requests
+import sys
+
+# get employee ID
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
+
+    # Fetch user info
+    user_info = requests.get(url).json()
+
+    # Fetch todo list
+    todo_info = requests.get(todo_url).json()
+
+    employee_name = user_info.get("name")
+
+    # Filter completed tasks
+    completed_tasks = [task for task in todo_info if task.get("completed")]
+    total_done = len(completed_tasks)
+    total_tasks = len(todo_info)
+
+    # Print the header
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, total_done, total_tasks))
+
+    # Print each completed task title
+    for task in completed_tasks:
+        print("\t {}".format(task.get("title")))
+
